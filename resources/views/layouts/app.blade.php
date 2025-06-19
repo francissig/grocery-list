8166f92f7f1f2e4a800c462c932c9c0580842f3f
francissig <franciss@live.ca>
2025-06-19 13:36:22 -0400

    style: adding navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Shopping List</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
 </head>
+
 <body>
-    <h1><a href="{{ route('shopping_lists.index') }}">Shopping Lists</a></h1>
+
+    <div class="navbar bg-base-100 shadow-sm">
+        <div class="navbar-start">
+
+            <a href="{{ route('shopping_lists.index') }}" class="btn btn-ghost text-xl">Shopping List</a>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+
+        </div>
+        <div class="navbar-center">
+            <a href="{{ route('shopping_lists.create') }}" class="btn">New List</a>
+        </div>
+    </div>
+
 
     @if (session('success'))
         <div>{{ session('success') }}</div>
@@ -15,4 +30,5 @@
 
     @yield('content')
 </body>
+
 </html>
