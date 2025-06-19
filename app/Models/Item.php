a7afff17b375970b56bd9dd52041259a1d3a8ce9
francissig <franciss@live.ca>
2025-06-19 12:41:19 -0400

    refactor: adding fillable to Item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
+    protected $fillable = ['name', 'category_id', 'shopping_list_id', 'purchased'];
+
     public function shoppingList()
     {
         return $this->belongsTo(ShoppingList::class);
