a5e1bc88360c148ad605b76960aa6d30ca55a365
francissig <franciss@live.ca>
2025-06-19 12:41:44 -0400

    refactor: adding fillable to ShoppingList model

--- app/Models/ShoppingList.php
+++ app/Models/ShoppingList.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShoppingList extends Model
 {
+    protected $fillable = ['title'];
+
     public function items()
     {
         return $this->hasMany(Item::class);
