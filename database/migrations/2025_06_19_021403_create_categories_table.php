623ec844d557039400c05d5af6655a7f2b418b31
francissig <franciss@live.ca>
2025-06-19 12:43:39 -0400

    refactor: minor changes to create method

--- database/migrations/2025_06_19_021403_create_categories_table.php
+++ database/migrations/2025_06_19_021403_create_categories_table.php
@@ -6,21 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->timestamps();
+            $table->string('name')->unique();
+            $table->timestamps(); // utile si tu veux savoir quand créé/modifié
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('categories');
